66a14ff36a5f21ef8ad462b3b2724139c69e879c
Zach Wolek <zach.wolek@gmail.com>
2025-01-08 14:57:28 -0600

    initial commit

---
+++ easy/contains-duplicate.js
@@ -0,0 +1,33 @@
+// Contains Duplicate
+// Given an integer array nums, return true if any value appears more than once in the array, otherwise return false.
+
+// Example 1:
+
+// Input: nums = [1, 2, 3, 3]
+
+// Output: true
+
+// Example 2:
+
+// Input: nums = [1, 2, 3, 4]
+
+// Output: false
+
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {boolean}
+     */
+    hasDuplicate(nums) {
+        for (let i = 0; i < nums.length; i++) {
+            for (let j = i + 1; j < nums.length; j++) {
+                if (nums[i] === nums[j]) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+}
+const solution = new Solution();
+console.log(solution.hasDuplicate([1, 2, 3, 3]));
