a98367658c4cca917139df0b04c46efc0bc4b3f2
Zach Wolek <zach.wolek@gmail.com>
2025-01-09 16:31:25 -0600

    create first answer

--- easy/valid-anagram.js
+++ easy/valid-anagram.js
@@ -1,3 +1,10 @@
 const isAnagram = (s, t) => {
-    
+    let sSort = s.split('').sort().join('')
+    let tSort = t.split('').sort().join('')
+    if (sSort === tSort){
+        return true
+    } else
+    return false
 }
+
+console.log(isAnagram("racecar", "carrace"))
